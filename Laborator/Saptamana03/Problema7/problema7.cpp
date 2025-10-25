046d68f578cb0f2cfc6391cd58545b72473fc5e2
Matei Sîrghe <73358042+Mach3tryhard@users.noreply.github.com>
2025-10-25 23:51:04 +0300

    Clean up and optimize matrix transpose logic
    Refactor code for better readability and efficiency.

--- Laborator/Saptamana03/Problema7/problema7.cpp
+++ Laborator/Saptamana03/Problema7/problema7.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-int a[105][105],b[105][105];
+int a[105][105], b[105][105];
 
-int  main()
-{
+int main() {
     int n;
     cin>>n;
     for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
+        for (int j=1;j<=n;j++)
             cin>>a[i][j];
-        }
-    }
     for(int i=1;i<=n;i++)
-    {
         for(int j=1;j<=n;j++)
-        {
-            b[j][i]=a[i][j];
-        }
-    }
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            cout<<b[i][j]<<' ';
-        }
-        cout<<'\n';
+            b[j][n-i+1]=a[i][j];
+    for(int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++)
+            cout<<b[i][j]<<" ";
+        cout<<"\n";
     }
     return 0;
 }
